97f15bbb661670bd028438cc8602d0aaee130fb4
Abiy Wasihun <abiywasihun0921@gmail.com>
2022-10-12 05:19:42 -0700

    sanity cms added to the app

--- components/categories.js
+++ components/categories.js
@@ -1,13 +1,36 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import CategoryCard from './CategoryCard'
 
-const categories = () => {
-    return ( <
-        View >
-        <
-        Text > categories < /Text> <
-        /View>
-    )
+const Categories = () => {
+  return (
+    <ScrollView horizontal
+    contentContainerStyle={{
+        paddingHorizontal:15,
+        paddingTop:10
+    }}
+    showsHorizontalScrollIndicator={false}>
+        {/**Category Card */}
+        <CategoryCard
+         imgurl="https://links.papareact.com/gn7" 
+         title="testing 1"/>
+        <CategoryCard 
+        imgurl="https://links.papareact.com/gn7" 
+        title="testing 2"/>
+        <CategoryCard 
+        imgurl="https://links.papareact.com/gn7" 
+        title="testing 3"/>
+        <CategoryCard
+         imgurl="https://links.papareact.com/gn7" 
+         title="testing 4"/>
+        <CategoryCard 
+        imgurl="https://links.papareact.com/gn7" 
+        title="testing 5"/>
+        <CategoryCard 
+        imgurl="https://links.papareact.com/gn7" 
+        title="testing 6"/>
+    </ScrollView>
+  )
 }
 
-export default categories
+export default Categories
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,24 +1,36 @@
 import { View, Text, SafeAreaView, Image, Platform, TextInput, ScrollView } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
-import {
-    SearchIcon,
-    AdujstmentsIcon,
-    UserIcon,
-    ChevronDownIcon,
-} from "react-native-heroicons/outline"
+import Categories from '../components/Categories';
+import FeaturedRow from '../components/FeaturedRow';
+import sanityClient from '../sanity'
+import { AdjustmentsHorizontalIcon, AdjustmentsVerticalIcon, ChevronDownIcon, MagnifyingGlassIcon, UserIcon } from 'react-native-heroicons/outline';
 export default function HomeScreen() {
     const navigation=useNavigation();
+    const [featuredCategories,setFeaturedCategories]=useState([])
     useLayoutEffect(()=>{
         navigation.setOptions({
             headerShown:false,
         })
     },[])
+useEffect(()=>{
+sanityClient.fetch(
+        `*[_type=="featured"]{
+            ...,
+            restaurants[]->{
+            ...,
+            dishes[]->
+            }
+            }
+        `
+).then(data=>{
+    setFeaturedCategories(data)
+})
+},[])
   return (
     <SafeAreaView className="bg-white pt-5">
-      <Text className="text-red-500">
       {/* Header*/}
-      <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
         <Image
         source={{
             uri:'https://links.papareact.com/wru'
@@ -36,20 +48,29 @@ export default function HomeScreen() {
         <UserIcon size={35} color="#00CCBB"/>
       </View>
       {/* Search */}
-      <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
-        <View className="flex-row space-x-2 bg-gray-200 p-3">
-            <SearchIcon color="gray" size={20}/>
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+            <MagnifyingGlassIcon color='gray' size={20}/>
             <TextInput placeholder='Restaurants and cuisines'
             keyboardType='default'/>
         </View>
-        <AdujstmentsIcon  color="#00CCBB"/>
+        <AdjustmentsVerticalIcon  color="#00CCBB"/>
         </View>
         {/* The Body */}
-        <ScrollView>
-            {/*Components */}
+        <ScrollView className="bg-gray-100">
+           <Categories/>
             {/*Featured Rows */}
+            {featuredCategories?.map(featured=>(
+            <FeaturedRow
+            key={featured._id}
+            id={featured._id}
+            title={featured.name}
+            description={featured.short_description}
+            />
+
+            ))}
+
         </ScrollView>
-      </Text>
     </SafeAreaView>
   )
 }
